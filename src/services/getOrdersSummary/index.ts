e68b060d7aefbe466867d043cbb0761de842a1c4
Personal <personalPersonal@tpmbp-tlmd6m.mynet>
2021-07-10 00:09:41 +0100

    Updated summary function

--- src/services/getOrdersSummary/index.ts
+++ src/services/getOrdersSummary/index.ts
@@ -7,22 +7,25 @@ interface OrdersSummary {
   sellOrders: string[];
 }
 
-const getOrdersSummary = (): OrdersSummary => {
+const getOrdersSummary = (fetchCount = 10): OrdersSummary => {
   return {
-    sellOrders: createOrderSummary(sellOrders).slice(0, 10),
-    buyOrders: createOrderSummary(buyOrders.reverse()).slice(0, 10),
+    sellOrders: createOrderSummary(sellOrders, fetchCount),
+    buyOrders: createOrderSummary(buyOrders.reverse(), fetchCount),
   };
 };
 
-const createOrderSummary = (currentOrders: Order[]) => {
+const createOrderSummary = (currentOrders: Order[], fetchCount: number) => {
   const ordersSummary: string[] = [];
 
   const groupedOrdersByPrice = _.groupBy(currentOrders, (o) => o.pricePerCoin);
 
   _.forIn(groupedOrdersByPrice, (orders, price) => {
+    if (ordersSummary.length === fetchCount) return false;
     const groupedOrdersByCoinType = _.groupBy(orders, (o) => o.coinType);
 
     _.forIn(groupedOrdersByCoinType, (orders, coinType) => {
+      if (ordersSummary.length === fetchCount) return false;
+
       const totalOrdersQuantity = _.sumBy(orders, (o) => o.orderQuantity);
       ordersSummary.push(`${totalOrdersQuantity} of ${coinType} for ${price}`);
     });
