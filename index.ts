89ea96544577ba41bcc98049fefce08c09a2eb22
Personal <personalPersonal@tpmbp-tlmd6m.mynet>
2021-07-10 01:13:50 +0100

    Add package json changes

--- index.ts
+++
@@ -1,6 +0,0 @@
-import cancelOrder from './src/services/cancelOrder';
-import getOrders from './src/services/getOrders';
-import placeOrder from './src/services/placeOrder';
-import getOrdersSummary from './src/services/getOrdersSummary';
-
-export { getOrders, placeOrder, cancelOrder, getOrdersSummary };
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import cancelOrder from './services/cancelOrder';
+import getOrders from './services/getOrders';
+import placeOrder from './services/placeOrder';
+import getOrdersSummary from './services/getOrdersSummary';
+
+export { getOrders, placeOrder, cancelOrder, getOrdersSummary };
