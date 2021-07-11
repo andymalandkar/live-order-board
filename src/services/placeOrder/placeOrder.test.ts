7f7a27382d4816b847a2cd04b336ad6a63dbb3f6
Personal <personalPersonal@tpmbp-tlmd6m.mynet>
2021-07-11 01:43:02 +0100

    Add tests for place order

---
+++ src/services/placeOrder/placeOrder.test.ts
@@ -0,0 +1,171 @@
+import placeOrder from '.';
+import { buyOrders, sellOrders } from '../../db';
+import { CoinType, Order, OrderType } from '../../types';
+
+describe('Place Order', () => {
+  beforeEach(() => {
+    buyOrders.length = 0;
+    sellOrders.length = 0;
+  });
+
+  describe('Buy Order', () => {
+    test('Should update buy orders and not sell orders', () => {
+      const orderId = placeOrder({
+        userId: 'user 1',
+        coinType: CoinType.Bitcoin,
+        orderQuantity: 100,
+        pricePerCoin: '£30',
+        orderType: OrderType.Buy,
+      });
+
+      expect(buyOrders).toEqual([
+        {
+          orderId,
+          userId: 'user 1',
+          coinType: CoinType.Bitcoin,
+          orderQuantity: 100,
+          pricePerCoin: '£30',
+          orderType: OrderType.Buy,
+        },
+      ]);
+
+      expect(sellOrders).toEqual([]);
+    });
+
+    test('Should be present in sorted order', () => {
+      const orders: Order[] = [
+        {
+          userId: 'user 1',
+          coinType: CoinType.Bitcoin,
+          orderQuantity: 100,
+          pricePerCoin: '£40',
+          orderType: OrderType.Buy,
+        },
+        {
+          userId: 'user 2',
+          coinType: CoinType.Ethereum,
+          orderQuantity: 100,
+          pricePerCoin: '£30',
+          orderType: OrderType.Buy,
+        },
+        {
+          userId: 'user 3',
+          coinType: CoinType.Bitcoin,
+          orderQuantity: 100,
+          pricePerCoin: '£20',
+          orderType: OrderType.Buy,
+        },
+      ];
+
+      orders[0].orderId = placeOrder(orders[0]);
+      orders[1].orderId = placeOrder(orders[1]);
+      orders[2].orderId = placeOrder(orders[2]);
+
+      expect(buyOrders).toMatchObject([orders[2], orders[1], orders[0]]);
+
+      expect(sellOrders).toEqual([]);
+    });
+  });
+
+  describe('Sell Order', () => {
+    test('Should update sell orders and not buy orders', () => {
+      const orderId = placeOrder({
+        userId: 'user 1',
+        coinType: CoinType.Bitcoin,
+        orderQuantity: 100,
+        pricePerCoin: '£30',
+        orderType: OrderType.Sell,
+      });
+
+      expect(sellOrders).toEqual([
+        {
+          orderId,
+          userId: 'user 1',
+          coinType: CoinType.Bitcoin,
+          orderQuantity: 100,
+          pricePerCoin: '£30',
+          orderType: OrderType.Sell,
+        },
+      ]);
+
+      expect(buyOrders).toEqual([]);
+    });
+
+    test('Should be present in sorted order', () => {
+      const orders: Order[] = [
+        {
+          userId: 'user 1',
+          coinType: CoinType.Bitcoin,
+          orderQuantity: 100,
+          pricePerCoin: '£40',
+          orderType: OrderType.Sell,
+        },
+        {
+          userId: 'user 2',
+          coinType: CoinType.Ethereum,
+          orderQuantity: 100,
+          pricePerCoin: '£30',
+          orderType: OrderType.Sell,
+        },
+        {
+          userId: 'user 3',
+          coinType: CoinType.Bitcoin,
+          orderQuantity: 100,
+          pricePerCoin: '£20',
+          orderType: OrderType.Sell,
+        },
+      ];
+
+      orders[0].orderId = placeOrder(orders[0]);
+      orders[1].orderId = placeOrder(orders[1]);
+      orders[2].orderId = placeOrder(orders[2]);
+
+      expect(sellOrders).toMatchObject([orders[2], orders[1], orders[0]]);
+
+      expect(buyOrders).toEqual([]);
+    });
+  });
+
+  describe('Buy & Sell Orders', () => {
+    test('Should match both buy & sell orders and should be in sorted order', () => {
+      const orders: Order[] = [
+        {
+          userId: 'user 1',
+          coinType: CoinType.Bitcoin,
+          orderQuantity: 100,
+          pricePerCoin: '£40',
+          orderType: OrderType.Buy,
+        },
+        {
+          userId: 'user 2',
+          coinType: CoinType.Ethereum,
+          orderQuantity: 100,
+          pricePerCoin: '£30',
+          orderType: OrderType.Sell,
+        },
+        {
+          userId: 'user 3',
+          coinType: CoinType.Bitcoin,
+          orderQuantity: 100,
+          pricePerCoin: '£20',
+          orderType: OrderType.Buy,
+        },
+        {
+          userId: 'user 3',
+          coinType: CoinType.Bitcoin,
+          orderQuantity: 100,
+          pricePerCoin: '£10',
+          orderType: OrderType.Sell,
+        },
+      ];
+
+      orders[0].orderId = placeOrder(orders[0]);
+      orders[1].orderId = placeOrder(orders[1]);
+      orders[2].orderId = placeOrder(orders[2]);
+      orders[2].orderId = placeOrder(orders[3]);
+
+      expect(buyOrders).toMatchObject([orders[2], orders[0]]);
+      expect(sellOrders).toMatchObject([orders[3], orders[1]]);
+    });
+  });
+});
